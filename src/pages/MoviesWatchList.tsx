3d9cbe2bd01459e599f336c1eb8fa87f281dcfde
Monica Carvajal <monik.182@gmail.com>
2024-11-24 13:16:52 +0100

    improve movies watch list page

--- src/pages/MoviesWatchList.tsx
+++ src/pages/MoviesWatchList.tsx
@@ -1,7 +1,7 @@
 import { Input } from 'antd'
 import { MovieList } from '../components'
 import { useMovieStorage } from '../hooks'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { EnhancedMovie } from '../types'
 
 export function MoviesWatchList() {
@@ -16,13 +16,21 @@ export function MoviesWatchList() {
     setFilteredMovies(filterMovies(text))
   }
 
+  useEffect(() => {
+    setFilteredMovies(movies)
+  }, [movies])
+
+  
   return (
     <div>
       <h1>Movies Watch List</h1>
       <Input.Search
-        onChange={(e) => handleOnChange(e.target.value)}
+        allowClear
+        type="text"
         style={{ width: 500 }}
+        onChange={(e) => handleOnChange(e.target.value)}
         placeholder="Search movie"
+        maxLength={100}
       />
       <MovieList movies={filteredMovies} onDelete={deleteMovie} onChange={updateMovie} pagination={{ pageSize: 100 }} />
     </div>
