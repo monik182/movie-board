0eb9a73fe6964cff3bd632080b3d1405f61015a9
Monica Carvajal <monik.182@gmail.com>
2024-11-24 12:41:46 +0100

    wrap app in session id provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,44 @@ import { AppContainer } from './App.styles'
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
 import { MoviesWatchList } from './MoviesWatchList'
 import { SearchMovies } from './SearchMovies'
-import { SessionIdProvider } from './hooks'
+import { SessionIdProvider, useSessionIdContext } from './hooks'
 import { Admin } from './Admin'
+import { LoadingOutlined } from '@ant-design/icons';
+import { Flex, Spin } from 'antd';
 
-const App: React.FC = () => {
+const AppContent: React.FC = () => {
+
+  const { sessionId } = useSessionIdContext()
+
+  if (!sessionId) {
+    return (
+      <Flex align="center" gap="middle">
+        <Spin indicator={<LoadingOutlined spin />} size="large" fullscreen tip="Loading..." />
+      </Flex>
+    )
+  }
 
   return (
     <Router>
-      <SessionIdProvider>
-        <AppContainer>
-          <nav>
-            <Link to="/">Movie Board</Link> | <Link to="/search">Search Movies</Link>
-          </nav>
-          <Routes>
-            <Route path="/" element={<MoviesWatchList />} />
-            <Route path="/search" element={<SearchMovies />} />
-            <Route path="/admin" element={<Admin />} />
-          </Routes>
-        </AppContainer>
-      </SessionIdProvider>
+      <AppContainer>
+        <nav>
+          <Link to="/">Movie Board</Link> | <Link to="/search">Search Movies</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<MoviesWatchList />} />
+          <Route path="/search" element={<SearchMovies />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </AppContainer>
     </Router>
+  )
+}
+
+const App: React.FC = () => {
+  return (
+    <SessionIdProvider>
+      <AppContent />
+    </SessionIdProvider>
   )
 }
 
